4e8232d2f4a2235d4925302bc19478186f95df2e
mevinx <mevin.chen@csu.fullerton.edu>
2018-12-12 16:26:40 -0800

    boolean no work

--- mcClient.py
+++ mcClient.py
@@ -9,28 +9,31 @@ serverPort = 12000
 
 #Create a socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
+value = True
+
 
 
 def downloadFile():
 	print ("What file would you like to download?")
 	downloadChoice = input()
-	clientSocket.connect((serverName, serverPort))
-	#clientSocket.send(input)
-	with open('received_file', 'wb') as f:
-		print("file opened")
-		while True:
-			print('receiving data...')
-			data = clientSocket.recv(1024)
-			#print('data=%s', (data))
-			if not data:
-				break
-			# write data to a file
-			f.write(data)
+	if value == True:
+		clientSocket.connect((serverName, serverPort))
 
-	f.close()
+	else:
+		with open('received_file', 'wb') as f:
+			print("file opened")
+			while True:
+				print('receiving data...')
+				data = clientSocket.recv(1024)
+				#print('data=%s', (data))
+				if not data:
+					break
+				# write data to a file
+				f.write(data)
+				
+		f.close()
 	print('Successfully get the file')
 	#replace under with input for switch later
-	clientSocket.close()
 	print('connection closed')
 	return
 
@@ -45,8 +48,32 @@ def listFile():
 	print("List file")
 
 def quitProgram():
-	print("quit program")
+	print("Quitting program")
+	clientSocket.close()
+	print("Connection now closed")
 	
+def choiceInput(inputChoice):
+
+	if inputChoice == "1":
+		downloadFile()
+		value = False
+		printChoices()
+		inputChoice = input()
+		choiceInput(inputChoice)
+	elif inputChoice == "2":
+		uploadFile()
+	elif inputChoice == "3":
+		listFile()
+	elif inputChoice == "4":
+		quitProgram()
+
+	
+def printChoices():
+	print ("Hello. What would you like to do?")
+	print ("1. Download file")
+	print ("2. Upload file")
+	print ("3. List files")
+	print ("4. Quit")	
 		
 
 
@@ -56,24 +83,11 @@ def quitProgram():
 #A string we want to send to the server
 data= "Hello world! This is a very long string."
 
-print ("Hello. What would you like to do?")
-print ("1. Download file")
-print ("2. Upload file")
-print ("3. List files")
-print ("4. Quit")
-
+printChoices()
 inputChoice = input()
-print (inputChoice)
+choiceInput(inputChoice)
 
-if inputChoice == "1":
-	downloadFile()
-elif inputChoice == "2":
-	uploadFile()
-elif inputChoice == "3":
-	listFile()
-elif inputChoice == "4":
-	quitProgram()
-print("yay")
+
 
 
 #Keep sending bytes until all bytes are sent
